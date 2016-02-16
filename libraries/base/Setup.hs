101a41d008e75652bcfaf244b4ff295ffa351002
John Ericson <Ericson2314@Yahoo.com>
2016-02-15 17:05:50 -0800

    base: Make sure Setup.hs doesn't get confused with new Prelude

--- libraries/base/Setup.hs
+++ libraries/base/Setup.hs
@@ -1,4 +1,8 @@
+-- Otherwise it tries to include Prelude from *this* base
+{-# LANGUAGE NoImplicitPrelude, PackageImports #-}
 module Main (main) where
+
+import "base" Prelude
 
 import Distribution.Simple
 

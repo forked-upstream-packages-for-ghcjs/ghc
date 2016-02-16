ed83072f86354c8a19f52bf4cd35bc9d33b40e14
John Ericson <Ericson2314@Yahoo.com>
2016-02-15 17:18:30 -0800

    base: don't use autoconf with GHCJS

--- libraries/base/Setup.hs
+++ libraries/base/Setup.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP #-}
 -- Otherwise it tries to include Prelude from *this* base
 {-# LANGUAGE NoImplicitPrelude, PackageImports #-}
 module Main (main) where
@@ -7,4 +8,8 @@ import "base" Prelude
 import Distribution.Simple
 
 main :: IO ()
+#ifdef ghcjs_HOST_OS
+main = defaultMain
+#else
 main = defaultMainWithHooks autoconfUserHooks
+#endif
